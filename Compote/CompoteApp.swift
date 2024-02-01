cf1bb5f17aed8e2714ea420060eb30d64d8fd095
Dorkside <jdmartin@gmx.fr>
2024-02-01 11:59:34 +0100

    Initial Commit

---
+++ Compote/CompoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompoteApp.swift
+//  Compote
+//
+//  Created by James MARTIN on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CompoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
